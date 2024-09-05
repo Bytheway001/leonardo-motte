f07c63b5f27f4c4be33610e8a958da98bf808ceb
Rafael Castillo <recastillo@eastrocksoftware.com>
2024-09-05 12:06:48 -0400

    Modification config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :contacts do
     # Ruta para obtener los matches de un contacto específico
-    get 'matches', to: 'matches#matches_for_contact', on: :member
+    get 'matches', on: :member
   end
 
   resources :roles do
     # Ruta para obtener los matches de un rol específico
-    get 'matches', to: 'roles#matches_for_role', on: :member
+    get 'matches', on: :member
   end
 
   # Define las rutas para crear, eliminar, listar y mostrar matches
