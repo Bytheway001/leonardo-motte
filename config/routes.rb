3dadb4905a6f112653ded463e8df7b501e4291fe
leonardo <leonardocastillo1410@gmail.com>
2024-09-16 16:02:59 -0400

    ready correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   resources :contacts do
-    get 'employment_histories', on: :member  # Custom route to fetch employment histories for a contact
-    resources :employment_histories, only: [:index, :show, :create, :update, :destroy]
-    get 'matches', on: :member
+    member do
+      get 'employment_histories'  
+      get 'matches'  
+    end
+
+   
+    resources :roles do
+      member do
+        get 'matches'  
+      end
+    end
+
+    resources :matches, only: [:create, :destroy, :index, :show]
   end
 
-  resources :roles do
-    get 'matches', on: :member
-  end
-
-  resources :matches, only: [:create, :destroy, :index, :show]
 
   resources :companies do
     resources :employment_histories, only: [:index, :show, :create, :update, :destroy]
